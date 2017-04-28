07c08d11439089da2bb0c26c34620cadc7dee449
Jonathan R. Williford <jonathan@neural.vision>
2017-04-28 14:23:50 +0200

    Switch to using dcm2niix (from dcm2nii).

--- bin/process_xnat_dicoms.py
+++ bin/process_xnat_dicoms.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+"""
+This script processes the DICOMS from the XNAT server. To use, add the 
+Process-NHP-MRI/bin directory to your PATH variable. You can then go to the directory
+you downloaded the XNAT DICOMS and run:
+
+    process_xnat_dicoms.py xnat [NII]
+
+where `xnat` is the XNAT downloaded directory and `NII` is the nifti output
+directory. If your current directory has the directory `xnat` all of the
+parameters are optional.
+"""
 
 import os
 import sys
@@ -6,6 +17,7 @@ import subprocess
 import pdb
 
 def system(cmd):
+    print(cmd)
     result = subprocess.check_output(cmd, shell=True)
     return result.decode('utf-8').splitlines()
 
@@ -15,7 +27,7 @@ def process_xnat_dicoms(xnat_dir, nii_output='NII'):
     os.makedirs(nii_output, exist_ok=True)
     for ddir in dicom_dirs:
         print('Processing %s' % ddir)
-        system('dcm2nii -g n -o %s %s/' % (nii_output, ddir))
+        system('dcm2niix -g n -o %s %s/' % (nii_output, ddir))
 
     system('gzip -f %s/*.nii' % nii_output)
 
@@ -28,4 +40,6 @@ if __name__ == '__main__':
         process_xnat_dicoms(sys.argv[1], sys.argv[2])
     else:
         print('Syntax:')
-        print('\t%s XNAT_DIR NII_OUTPUT' % os.path.basename(sys.argv[0]))
+        print('\t%s XNAT_DIR [NII_OUTPUT]' % os.path.basename(sys.argv[0]))
+        print('\nNII_OUTPUT defaults to NII')
+        print('XNAT_DIR is option if current directory contains an "xnat" directory.')
