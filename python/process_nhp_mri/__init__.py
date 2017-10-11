88ba05f4d07af54c39fa3cec5e3bc8bbab86504f
Jonathan R. Williford <jonathan@neural.vision>
2017-10-11 11:23:01 +0200

    Add images for FNIRT documentation.

--- python/process_nhp_mri/__init__.py
+++ python/process_nhp_mri/__init__.py
@@ -3,3 +3,4 @@ from .setup_fsf_lev1 import setup_fsf_lev1
 from .run_all_fsf_lev1 import run_all_fsf_lev1
 from .reorient_functs import reorient_functs
 from .get_motion_outliers import get_motion_outliers
+from .QA_lev1s import lev1_QA_html
