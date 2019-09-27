1a3bb8489406859f60d5f41898d66adf3ba013e7
Chris Klink <p.c.klink@gmail.com>
2019-09-27 16:52:11 +0200

    second step in task label changes

--- bin/rename_task_bids.sh
+++ bin/rename_task_bids.sh
@@ -3,19 +3,36 @@
 ROOT=/NHP_MRI/NHP-BIDS/
 
 # rename tasks
+
+# figure ground
 declare -a typefld=(func)
 for t in "${!typefld[@]}"; do
-    find ${ROOT} -name *${t}*task-figureground* -exec bash -c 'mv "$1" "${1/task-figureground/task-figgnd}"' -- {} \;
+    find ${ROOT} -type d -name *${t}*task-figureground* -exec bash -c 'mv "$1" "${1/task-figureground/task-figgnd}"' -- {} \;
 done
 
-# rename tasks
 declare -a typefld=(func)
 for t in "${!typefld[@]}"; do
-    find ${ROOT} -name *${t}*task-figureground_localizer* -exec bash -c 'mv "$1" "${1/task-figureground_localizer/task-figgndloc}"' -- {} \;
+    find ${ROOT} -type f -name *${t}*task-figureground* -exec bash -c 'mv "$1" "${1/task-figureground/task-figgnd}"' -- {} \;
 done
 
-# rename tasks
+# figure ground localizers
 declare -a typefld=(func)
 for t in "${!typefld[@]}"; do
-    find ${ROOT} -name *${t}*task-restingstate* -exec bash -c 'mv "$1" "${1/task-restingstate/task-rest}"' -- {} \;
+    find ${ROOT} -type d -name *${t}*task-figgnd_localizer* -exec bash -c 'mv "$1" "${1/task-figgnd_localizer/task-figgndloc}"' -- {} \;
+done
+
+declare -a typefld=(func)
+for t in "${!typefld[@]}"; do
+    find ${ROOT} -type f -name *${t}*task-figgnd_localizer* -exec bash -c 'mv "$1" "${1/task-figgnd_localizer/task-figgndloc}"' -- {} \;
+done
+
+# resting state
+declare -a typefld=(func)
+for t in "${!typefld[@]}"; do
+    find ${ROOT} -type d -name *${t}*task-restingstate* -exec bash -c 'mv "$1" "${1/task-restingstate/task-rest}"' -- {} \;
+done
+
+declare -a typefld=(func)
+for t in "${!typefld[@]}"; do
+    find ${ROOT} -type f -name *${t}*task-restingstate* -exec bash -c 'mv "$1" "${1/task-restingstate/task-rest}"' -- {} \;
 done
