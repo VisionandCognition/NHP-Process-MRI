2702937cb5516562d5c9ec60b2e7e3dcca9db839
Chris Klink <p.c.klink@gmail.com>
2019-11-22 12:15:15 +0100

    Script improvements

--- bin/rename_scan2run_bids.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-ROOT=/NHP_MRI/NHP-BIDS/
-
-# replace scan with run in anat/fmap/dwi folders
-declare -a typefld=(anat fmap dwi)
-for t in "${!typefld[@]}"; do
-    find ${ROOT} -name *${t}*scan* -type f -exec bash -c 'mv "$1" "${1/scan/run}"' -- {} \;
-done
--- bin/swap_xdir_voxels.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo 'Swapping x voxel order'
-for f in $(find ./* -name '*.nii.gz'); do 
-	fslswapdim $f -x y z $f
-done
-echo 'Done'
