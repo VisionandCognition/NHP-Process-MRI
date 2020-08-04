2b3fab3caf2a7771369bdae64ff6a814c8c55a6d
neurosutton <21200969+neurosutton@users.noreply.github.com>
2020-08-04 09:41:42 -0600

    Fix random slice drop due to sorting
    (1) rework the logic for dropping a file
    (2) clean up extra comments and code

--- python/fix_dcm_incompletevols.py
+++ python/fix_dcm_incompletevols.py
@@ -1,7 +1,10 @@
 # Call this script with the path to your dicom folder as argument
 # e.g., python fix_dcm_incompletevols.py dicom_folder
 # p.c.klink@gmail.com
-# Generalizations and modifications to the comparison logic added by neurosutton
+#
+# Changes
+# July 2020 - Generalizations and modifications to the comparison logic added by B. Sutton
+# Cropping the series after identifying the minimum number of full acquisitions by reading the dicom header and limiting on AcquisitionNumber prevents sorting issues, which may lead to dropping slices mid-series... messing up the conversion to nifti
 
 import os, sys, shutil, glob
 import pydicom
@@ -16,6 +19,7 @@ if len(sys.argv) > 1:
 
     # check each possible entry to make sure the file is a dicom file
     dcm_list = [f for f in possible_files if f.split('.')[-1].lower() in valid_dcm_extns]
+    dcm_list.sort()
     if not dcm_list:
         print('Did not find dicom files to convert.\n')
     else:
@@ -26,62 +30,53 @@ if len(sys.argv) > 1:
         for f in dcm_list:
             try:
                 dcm_info = pydicom.filereader.dcmread(f,stop_before_pixels=True)
-                #volnum.append(dcm_info.TemporalPositionIdentifier)
+                #volnum.append(dcm_info.TemporalPositionIdentifier) # may work on some systems
                 volnum.append(dcm_info.SliceLocation)
             except Exception as e:
                 print('{}: {}'.format(f,e))
 
-        # how many slices in each volume
+        # how many slices in each volume; identify the fewest number of repeats
         vol_list=[]
-        # setup a dictionary to handle all the slice locations and number of acquisitions at that location
-        vol_dict = {}
         for i in set(volnum):
-            vol_dict[volnum] = volnum.count(i)
             vol_list.append(volnum.count(i))
 
-        # get the indexes of the slices belonging to the last (incomplete) volume
-        if max(vol_list) != min(vol_list): # First index vs last may not work, depending on acquisition scheme (i.e., even-first, interleaved)
-            last_full_acq = min(vol_list)*len(vol_list)
+        if max(vol_list) != min(vol_list): # Test for a full collection
+            last_full_acq = min(vol_list)*len(vol_list) # Determine the total number of files after cropping
             print('Using {} files ({} slices*{} complete volumes)'.format(last_full_acq, len(vol_list), min(vol_list)))
 
-            # Reset the max counter for each of the slice locations to the minimum number of complete acquisitions.
-            for k in vol_dict.keys():
-                vol_dict[k] = min(vol_list)
             corr_dcm_list = []
             orphaned_list = []
 
             # Find the number of acquisitions for each slice that correspond to the minimum number of full volumes acquired.
-            for v, dcm_file in enumerate(vol):
+            for dcm_file in dcm_list:
                 try:
                     dcm_info = pydicom.filereader.dcmread(dcm_file,stop_before_pixels=True)
-                    vol_dict[dcm_info.SliceLocation] = vol_dict[dcm_info.SliceLocation] - 1
-                    if vol_dict[dcm_info.SliceLocation] >= 0:
-                        corr_dcm_list.append(dcm_file)
-                    else:
-                        print('Delete:{}'.format(dcm_info.SliceLocation))
+                    if not dcm_info.SliceLocation:
+                        print('{} part of scout?'.format(dcm_file))
                         orphaned_list.append(dcm_file)
+                    elif dcm_info.AcquisitionNumber > min(vol_list):
+                        orphaned_list.append(dcm_file)
+                    else:
+                        corr_dcm_list.append(dcm_file)
                 except Exception as e:
                     print('{}: {}'.format(f,e))
 
-            print('The following slice files will be ignored')
-            print(orphaned_list)
-
+            # Create directory structures
             if os.path.isdir(os.path.join(dcmpath , 'orphan_dcm')) is False:
                 os.mkdir(os.path.join(dcmpath , 'orphan_dcm'))
             if os.path.isdir(os.path.join(dcmpath , 'corrected_dcm')) is False:
                 os.mkdir(os.path.join(dcmpath , 'corrected_dcm'))
 
             print('Moving {} orphan dcm files to orphan_dcm'.format(len(orphaned_list)))
-            # print('Moving the following orphan dcm files to orphan_dcm')
-            for f in orphaned_list:
-                #print(dcm_list[f])
-                orphan_filename = os.path.split(orphaned_list[f])
-                shutil.move(orphaned_list[f],os.path.join(dcmpath , 'orphan_dcm' , orphan_filename[1]))
+            for orphan in orphaned_list:
+                orphan_filename = os.path.split(orphan)
+                shutil.move(orphan,os.path.join(dcmpath , 'orphan_dcm' , orphan_filename[1]))
 
 
             print('Moving the rest of the dcm files to corrected_dcm')
             for f in corr_dcm_list:
-                shutil.move(f,os.path.join(dcmpath , 'corrected_dcm',))
+                corr_filename = os.path.split(f)
+                shutil.move(f,os.path.join(dcmpath , 'corrected_dcm', corr_filename[1]))
         else:
             print('All volumes are complete, will not mess with dcm files.')
 else:
